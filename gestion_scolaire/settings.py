8a1d73e595e442ac41b562a6c8d35b61a00f6542
DRABO Yasmine <draboyasmine11@gmail.com>
2026-02-25 12:16:02 +0000

    Correction du bug sur la page login et mise à jour du README

--- gestion_scolaire/settings.py
+++ gestion_scolaire/settings.py
@@ -10,8 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
-from pathlib import Path
 import os
+from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -82,11 +82,7 @@ WSGI_APPLICATION = "gestion_scolaire.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": "django",
-        'USER': 'django_user',            
-        'PASSWORD': 'django', 
-        'HOST': 'localhost',            
-        'PORT': '3306',                 
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
